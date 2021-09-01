12681bf5e655085867933a04ea97c445fa640157
vatsal999 <68118780+vatsal999@users.noreply.github.com>
2021-09-01 23:45:41 +0530

    add sfml

---
+++ sfml/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // create window
+    //sf::Window window(sf::VideoMode(x,y),"TITLE",sf::style::Fullscreen);
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "MyApp");
+
+    // make application refresh rate equal to monitor
+    //window.setVerticalSyncEnabled(true);
+
+    // set application refresh rate
+    window.setFramerateLimit(60);
+
+    sf::Texture rocketTexture;
+    rocketTexture.loadFromFile("./rocket.png");
+
+    sf::Sprite rocket(rocketTexture);
+
+    sf::Vector2u size = rocketTexture.getSize();
+    rocket.setOrigin(size.x / 2, size.y / 2);
+    sf::Vector2f increment(0.4f, 0.4f);
+
+    
+
+
+
+    //
+    //run program as long as window in open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+
+
+        //
+        //MOUSE EVENTS
+        //
+        /* if (event.type == sf::Event::MouseMoved) */
+        /* { */
+        /*     std::cout << "new mouse x: " << event.mouseMove.x << std::endl; */
+        /*     std::cout << "new mouse y: " << event.mouseMove.y << std::endl; */
+        /* } */
+
+
+        while (window.pollEvent(event))
+        {
+            // close window when requested
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        if((rocket.getPosition().x + (size.x / 2) >
+             window.getSize().x && increment.x > 0) ||
+            (rocket.getPosition().x - (size.x / 2) < 0 &&
+            increment.x < 0))
+                {
+                // Reverse the direction on X axis.
+                increment.x = -increment.x;
+                }
+
+        if((rocket.getPosition().y + (size.y / 2) >
+            window.getSize().y && increment.y > 0) ||
+            (rocket.getPosition().y - (size.y / 2) < 0 && increment.y < 0))
+                {
+                // Reverse the direction on Y axis.
+                increment.y = -increment.y;
+                }
+
+        rocket.setPosition(rocket.getPosition() + increment);
+
+
+        /* window.clear(sf::Color::White); */
+        window.clear();
+
+        window.draw(rocket);
+
+        window.display();
+    }
+    return 0;
+
+
+}
---
+++ sfml/readme.md
@@ -0,0 +1,12 @@
+## COMPILING
+
+### Dependencies:
+sfml
+
+
+```Shell
+g++ -c main.cpp
+g++ main.o -o MyApp -lsfml-graphics -lsfml-window -lsfml-system
+```
+
+
